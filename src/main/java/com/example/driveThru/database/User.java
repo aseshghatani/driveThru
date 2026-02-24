47203304254446057bb2a537d4a7fce0328b6e77
aseshghatani <aseshghatani4@gmail.com>
2026-02-24 15:53:31 +0530

    user_entity

--- src/main/java/com/example/driveThru/database/User.java
+++
@@ -1,6 +0,0 @@
-package com.example.driveThru.database;
-
-public class User {
-
-}
-
---
+++ src/main/java/com/example/driveThru/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.driveThru.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String name;
+    private String email;
+    private String phone;
+    private String is_veg;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIs_veg() {
+        return is_veg;
+    }
+
+    public void setIs_veg(String is_veg) {
+        this.is_veg = is_veg;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
+
